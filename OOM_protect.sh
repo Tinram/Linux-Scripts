cd10004025f7c0691f1de558ed0bee1264a90a59
Tinram <Tinram@users.noreply.github.com>
2018-06-26 21:13:03 +0100

    update OOM_protect.sh for mysql

--- OOM_protect.sh
+++ OOM_protect.sh
@@ -8,26 +8,36 @@
 #                 sudo ./OOM_protect.sh
 #
 # Copyright:      Martin Latter, 20/06/2018
-# Version:        0.01
+# Version:        0.02
 # License:        GNU GPL version 3.0 (GPL v3); http://www.gnu.org/licenses/gpl.html
 # Link:           https://github.com/Tinram/Linux-Scripts.git
 
 
 
-######################
+#############################
 # CONFIG
-# set app path
-######################
-APP=/usr/bin/<app>
-######################
+# set application path
+#############################
+APP=/usr/local/bin/gpggui   # set path to your app
+#APP=/usr/bin/mysql         # mysql example
+#############################
 
 
 sysctl vm.oom_kill_allocating_task=1 # set OOM: culprit = first victim
 
-$APP & export PID=$!
+
+case "$APP" in
+
+	*mysql*)
+		PID=$(pgrep mysql) ;;
+
+	*)
+		$APP & export PID=$! ;;
+esac
+
 
 echo "PID: $PID"
 
 echo -1000 > /proc/$PID/oom_score_adj # -1000 is exempt from OOM Killer
 
-echo -n "OOM Score of PID $PID: " && cat /proc/$PID/oom_score_adj # confirm
+echo -n "OOM score of PID $PID: " && cat /proc/$PID/oom_score_adj # confirm
